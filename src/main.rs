19bd27a069ef40f0233b07ab5c379c92b46778d1
Robin Husman <robinlhusman@gmail.com>
2025-10-01 03:16:17 -0400

    Basically running the eframe template app

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,77 @@
-use leptos::{logging, prelude::*};
-use leptos_router::{
-    components::{Route, Router, Routes},
-    hooks::use_query,
-    params::Params,
-    path,
-};
+//use crate::shader_editors::ShaderEditorFromId;
 
-use crate::shader_editors::ShaderEditorFromId;
+//pub mod shader_editor;
+//pub mod shader_editors;
 
-pub mod shader_editor;
-pub mod shader_editors;
+pub mod app;
 
-#[component]
-fn App() -> impl IntoView {
-    // https://github.com/ramosbugs/openidconnect-rs/blob/main/examples/google.rs
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    use crate::app::App;
 
-    view! {
-        <Router>
-            <Routes fallback=|| {
-                view! { "Routing was unsuccessful" }
-            }>
-                <Route path=path!(":id") view=ShaderEditorFromId />
-            </Routes>
-        </Router>
-    }
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]), //.with_icon(
+        // NOTE: Adding an icon is optional
+        //eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+        //    .expect("Failed to load icon"),
+        //)
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    )
 }
 
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
 fn main() {
-    console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| App())
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(crate::app::App::new(cc)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
